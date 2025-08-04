f507fa30062137ab7dcd30c9d9a0ce059c17755d
Vedhamshbode <vedhamshbode@gmail.com>
2025-08-04 11:44:48 +0530

    thrid commit

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     open:true,
     port:3001,
   },
-  base: "/portfolio_websitenew/",
+  base: "/vedhamsh-portfolionew/",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
